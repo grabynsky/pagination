b975dc43289beabc367fbd62e312a0ad94f75cdf
Grabynsky Andrew <hrabynsky@gmail.com>
2025-02-27 22:06:29 +0200

    end pad

--- src/components/TodosComponent/TodosComponent.tsx
+++ src/components/TodosComponent/TodosComponent.tsx
@@ -1,12 +1,36 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {ITodo} from "../../models/ITodo";
 import TodoComponent from "../Todo/TodoComponent";
+import {useSearchParams} from "react-router-dom";
+import {apiService} from "../../services/api/api.service";
 
 type TodosProps = {
     todos: ITodo[];
 }
 
-const TodosComponent: FC<TodosProps> = ({todos}) => {
+const TodosComponent: FC<TodosProps> = () => {
+
+    const [query, setQuery] = useSearchParams({page: '1'});
+    const [todos, setTodos] = useState<ITodo[]>([]);
+    const [flag, setFlag] = useState<boolean>(false);
+
+    useEffect(() => {
+        const page = query.get('page');
+
+        //todo fn to call API with come prams
+        if (page) {
+            apiService.todo
+                .getAll(+page)
+                .then(value => {
+                        setTodos(value.todos);
+                        const lastId = value.todos[value.todos.length - 1].id;
+                        lastId >= value.total ? setFlag(true) : setFlag(false);
+                    }
+                );
+
+        }
+
+    }, [query]);
     return (
         <div>
             {
